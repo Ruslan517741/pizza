6bd030ae1968c8228b50d1db13e554c36ee39d62
Rus <bedlinskiyruslan@gmail.com>
2022-10-04 15:26:02 +0300

    fix bug with media400px

--- src/components/errorMessage/ErrorMessage.js
+++ src/components/errorMessage/ErrorMessage.js
@@ -1,8 +1,9 @@
 import error404 from '../../resources/img/error404.jpg';
+import './errorMessage.scss';
 
 const ErrorMessage = () => {
     return (
-        <img style={{display: 'block', width: '500px', height: '500px', objectFit:'contain', margin: '0 auto'}} src={error404} alt="Error"/>
+        <img className='error404' /* style={{display: 'block', width: '500px', height: '500px', objectFit:'contain', margin: '0 auto'}} */ src={error404} alt="Error"/>
     )
 }
 
